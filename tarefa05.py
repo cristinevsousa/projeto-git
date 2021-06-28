9450638fbf28d95ad7b3b0ac13cee766dd2d668c
Cristine <cristinevsousa@gmail.com>
2021-06-28 15:14:39 -0300

    Update 3 - Tarefa05

--- tarefa05.py
+++ tarefa05.py
@@ -2,10 +2,8 @@
 # A função deve imprimir todos os elementos da lista, enumerando-os.
 
 def enum(lista):
-    i = 0
-    while i < len(lista):
+    for i in range(len(lista)):
         print(i+1, "-", lista[i])
-        i += 1
 
 
 lista = ["abc", 2, 3.5, True]
